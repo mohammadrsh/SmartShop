cf81a26724904db456bd22b69ae019bdf3021adf
Mohammadrsh <mohammadrsh@yahoo.com>
2017-06-28 16:46:14 +0430

    some class added!

---
+++ SmartShop/Db.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartShop
+{
+    class Db
+    {
+        public static SmartShopEntities ume;
+        public static SmartShopEntities getInstance()
+        {
+            if (ume == null)
+            {
+                ume = new SmartShopEntities();
+                return ume;
+            }
+            else
+            {
+                return ume;
+            }
+
+        }
+    }
+}
