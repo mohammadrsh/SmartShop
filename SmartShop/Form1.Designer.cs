7ccd84a2b593818959a66e4eb0fc03be29efe091
Elahe <Elahe@DESKTOP-IOFQ4B5>
2017-06-29 19:20:27 +0430

    EditForm1

--- SmartShop/Form1.Designer.cs
+++ SmartShop/Form1.Designer.cs
@@ -35,33 +35,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(160, 88);
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, -1);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(303, 26);
+            this.label1.Size = new System.Drawing.Size(311, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Our Shop system";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(224, 175);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(430, 51);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 49);
+            this.button1.Size = new System.Drawing.Size(110, 29);
             this.button1.TabIndex = 1;
             this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 324);
+            this.BackgroundImage = global::SmartShop.Properties.Resources.online_shopping_ecommerce_ss_1920;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(967, 324);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Welcome!";
             this.Click += new System.EventHandler(this.Form1_Load);
--- SmartShop/FrmLogIn.Designer.cs
+++ SmartShop/FrmLogIn.Designer.cs
@@ -43,9 +43,10 @@
             // btnLogIn
             // 
             this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.btnLogIn.Location = new System.Drawing.Point(316, 292);
+            this.btnLogIn.Location = new System.Drawing.Point(211, 190);
+            this.btnLogIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(122, 49);
+            this.btnLogIn.Size = new System.Drawing.Size(81, 32);
             this.btnLogIn.TabIndex = 13;
             this.btnLogIn.Text = "Log In";
             this.btnLogIn.UseVisualStyleBackColor = true;
@@ -53,16 +54,18 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(316, 231);
+            this.txtPassword.Location = new System.Drawing.Point(211, 150);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(121, 26);
+            this.txtPassword.Size = new System.Drawing.Size(82, 20);
             this.txtPassword.TabIndex = 12;
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(316, 178);
+            this.txtUserName.Location = new System.Drawing.Point(211, 116);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(121, 26);
+            this.txtUserName.Size = new System.Drawing.Size(82, 20);
             this.txtUserName.TabIndex = 11;
             // 
             // combUserType
@@ -72,18 +75,20 @@
             "Admin",
             "StockClerk",
             "SellClerk"});
-            this.combUserType.Location = new System.Drawing.Point(316, 122);
+            this.combUserType.Location = new System.Drawing.Point(211, 79);
+            this.combUserType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.combUserType.Name = "combUserType";
-            this.combUserType.Size = new System.Drawing.Size(121, 28);
+            this.combUserType.Size = new System.Drawing.Size(82, 21);
             this.combUserType.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label3.Location = new System.Drawing.Point(172, 125);
+            this.label3.Location = new System.Drawing.Point(115, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 25);
+            this.label3.Size = new System.Drawing.Size(93, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "User Type :";
             // 
@@ -91,9 +96,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(172, 237);
+            this.label2.Location = new System.Drawing.Point(115, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.Size = new System.Drawing.Size(87, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Password :";
             // 
@@ -101,9 +107,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(172, 178);
+            this.label1.Location = new System.Drawing.Point(115, 116);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.Size = new System.Drawing.Size(98, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "User Name :";
             // 
@@ -111,9 +118,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(128, 9);
+            this.label4.Location = new System.Drawing.Point(85, 6);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(378, 94);
+            this.label4.Size = new System.Drawing.Size(255, 62);
             this.label4.TabIndex = 14;
             this.label4.Text = "smart shop";
             // 
@@ -121,9 +129,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(99, 368);
+            this.label5.Location = new System.Drawing.Point(66, 239);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(229, 25);
+            this.label5.Size = new System.Drawing.Size(173, 17);
             this.label5.TabIndex = 15;
             this.label5.Text = "Dont have an acount ?";
             // 
@@ -132,9 +141,10 @@
             this.lblSignUp.AutoSize = true;
             this.lblSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.lblSignUp.LinkColor = System.Drawing.Color.DodgerBlue;
-            this.lblSignUp.Location = new System.Drawing.Point(364, 368);
+            this.lblSignUp.Location = new System.Drawing.Point(243, 239);
+            this.lblSignUp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSignUp.Name = "lblSignUp";
-            this.lblSignUp.Size = new System.Drawing.Size(86, 25);
+            this.lblSignUp.Size = new System.Drawing.Size(63, 17);
             this.lblSignUp.TabIndex = 16;
             this.lblSignUp.TabStop = true;
             this.lblSignUp.Text = "Sign up";
@@ -142,11 +152,11 @@
             // 
             // FrmLogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(609, 448);
+            this.ClientSize = new System.Drawing.Size(401, 291);
             this.Controls.Add(this.lblSignUp);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -157,6 +167,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmLogIn";
             this.Text = "FrmLogIn";
             this.ResumeLayout(false);
