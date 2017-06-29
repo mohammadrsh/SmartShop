3eb80e046b14e1ccf20896587f3880330bd1638c
Elahe <Elahe@DESKTOP-IOFQ4B5>
2017-06-30 01:08:20 +0430

    Add_InvoiceManagementFrm

--- SmartShop/FrmSellerInvoiceManagement.Designer.cs
+++ SmartShop/FrmSellerInvoiceManagement.Designer.cs
@@ -28,19 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EnterBtn = new System.Windows.Forms.Button();
+            this.CustomerIDtxt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.InvoiceNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InvoiceDateAndTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CashierNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CustomerNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CustomerIDtxt);
+            this.groupBox1.Controls.Add(this.EnterBtn);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Customer ID:";
+            // 
+            // EnterBtn
+            // 
+            this.EnterBtn.Location = new System.Drawing.Point(236, 12);
+            this.EnterBtn.Name = "EnterBtn";
+            this.EnterBtn.Size = new System.Drawing.Size(75, 23);
+            this.EnterBtn.TabIndex = 0;
+            this.EnterBtn.Text = "Enter";
+            this.EnterBtn.UseVisualStyleBackColor = true;
+            // 
+            // CustomerIDtxt
+            // 
+            this.CustomerIDtxt.Location = new System.Drawing.Point(120, 12);
+            this.CustomerIDtxt.Name = "CustomerIDtxt";
+            this.CustomerIDtxt.Size = new System.Drawing.Size(100, 20);
+            this.CustomerIDtxt.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.InvoiceNumber,
+            this.InvoiceDateAndTime,
+            this.CashierNumber,
+            this.CustomerNumber,
+            this.ProductName,
+            this.Price});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(431, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // InvoiceNumber
+            // 
+            this.InvoiceNumber.HeaderText = "Invoice Number";
+            this.InvoiceNumber.Name = "InvoiceNumber";
+            // 
+            // InvoiceDateAndTime
+            // 
+            this.InvoiceDateAndTime.HeaderText = "Date And Time";
+            this.InvoiceDateAndTime.Name = "InvoiceDateAndTime";
+            // 
+            // CashierNumber
+            // 
+            this.CashierNumber.HeaderText = "Cashier Number";
+            this.CashierNumber.Name = "CashierNumber";
+            // 
+            // CustomerNumber
+            // 
+            this.CustomerNumber.HeaderText = "Cusromer Number";
+            this.CustomerNumber.Name = "CustomerNumber";
+            // 
+            // ProductName
+            // 
+            this.ProductName.HeaderText = "Product Name";
+            this.ProductName.Name = "ProductName";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
             // 
             // FrmSellerInvoiceManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 273);
+            this.ClientSize = new System.Drawing.Size(767, 314);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmSellerInvoiceManagement";
             this.Text = "FrmSellerInvoiceManagement";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox CustomerIDtxt;
+        private System.Windows.Forms.Button EnterBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InvoiceNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InvoiceDateAndTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CashierNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
     }
 }
