9368b2f3e5c345e33ed8d2f50e90637700f93c51
Mohammadrsh <mohammadrsh@yahoo.com>
2017-06-29 17:10:37 +0430

    Controller updated!

--- SmartShop/Controller.cs
+++ SmartShop/Controller.cs
@@ -30,11 +30,36 @@ namespace SmartShop
             context.SaveChanges();
         }
 
+        public Transaction getTransaction(int id)
+        {
+            var context = Db.getInstance();
+            Transaction trans = context.Transactions.FirstOrDefault(t => t.id == id);
+            return trans;
+        }
+
         public void addCustomer(Customer c)
         {
             var context = Db.getInstance();
             context.Customers.Add(c);
             context.SaveChanges();
+        }
+
+        public List<Kala> getKalas()
+        {
+            var context = Db.getInstance();
+            return context.Kalas.ToList();
+        }
+
+        public List<Transaction> getTransactions()
+        {
+            var context = Db.getInstance();
+            return context.Transactions.ToList();
+        }
+    
+        public List<Customer> getCustomers()
+        {
+            var context = Db.getInstance();
+            return context.Customers.ToList();
         }
     }
 }
