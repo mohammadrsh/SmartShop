88c2194be78b15f0ad4b817af3f88bc25f1ef5a2
Elahe <Elahe@DESKTOP-IOFQ4B5>
2017-06-30 00:51:38 +0430

    EditSellerForm

--- SmartShop/FrmSellerInvoiceManagement.cs
+++ SmartShop/FrmSellerInvoiceManagement.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace SmartShop
 {
-    public partial class FrmSellClerk : Form
+    public partial class FrmSellerInvoiceManagement : Form
     {
-        public FrmSellClerk()
+        public FrmSellerInvoiceManagement()
         {
             InitializeComponent();
-        }
-
-        private void SellClreckfrm_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
